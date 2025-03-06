7a8bf97e2d2d8d4dbf9455b787975b54609e091d
Ezra Hall <ezrajhall0@gmail.com>
2025-03-06 11:29:36 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector(".cta-button");
+    button.addEventListener("click", () => {
+        alert("Let's check out your projects!");
+    });
+});
